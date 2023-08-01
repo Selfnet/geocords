ef77060d0acebd2a5c090ff211ba9cc0825fb66b
Philipp <philipp@selfnet.de>
2023-08-01 14:34:46 +0200

    init vite-solidts-project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <h1>Vite + Solid</h1>
+      <p class="read-the-docs">
+        Click on the Vite and Solid logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
